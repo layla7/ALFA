580d84c59321a346d80c23c510388611141ea720
Sanghyuk Lee <xxxblanky@gmail.com>
2021-01-21 19:30:27 +0900

    Modified for resuming wandb

--- experiment_scripts/alfa+maml.sh
+++ experiment_scripts/alfa+maml.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-export GPU_ID=$1
+export WANDB_ID=$1
+export GPU_ID=$2
 
 echo $GPU_ID
 
 cd ..
 export DATASET_DIR="datasets/"
-export CUDA_VISIBLE_DEVICES=$1,$2,$3,$4
+export CUDA_VISIBLE_DEVICES=$2,$3,$4,$5
 # Activate the relevant virtual environment:
-python train_maml_system.py --name_of_args_json_file experiment_config/alfa+maml.json --gpu_to_use $GPU_ID
+python train_maml_system.py --name_of_args_json_file experiment_config/alfa+maml.json --gpu_to_use $GPU_ID --wandb_id $WANDB_ID
